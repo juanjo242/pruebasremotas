90dcfaf9bca074e46789fd05bc93beb682e2d613
Juan José Chacón <Juan Jos Chacn@DESKTOP-JUANJO>
2024-05-29 10:54:00 +0200

    He añadido adios cocodrilo

---
+++ EjercicioGit2/src/identificacion/identificador.java
@@ -0,0 +1,10 @@
+package identificacion;
+
+public class identificador {
+
+	public static void main(String[] args) {
+		System.out.println("Hola caracola");
+		System.out.println("adios cocodrilo");
+	}
+
+}
