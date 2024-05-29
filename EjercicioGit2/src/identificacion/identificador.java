7d458ee2685d62b704f5271d90a7333b4d699b82
Juan José Chacón <Juan Jos Chacn@DESKTOP-JUANJO>
2024-05-29 11:04:17 +0200

    he añadido pepsicola

--- EjercicioGit2/src/identificacion/identificador.java
+++ EjercicioGit2/src/identificacion/identificador.java
@@ -5,6 +5,7 @@ public class identificador {
 	public static void main(String[] args) {
 		System.out.println("Hola caracola");
 		System.out.println("adios cocodrilo");
+		System.out.println("Hola pepsicola");
 	}
 
 }
