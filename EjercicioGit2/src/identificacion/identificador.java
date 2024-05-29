30c3f20a8132cfc449320958e34f9727deef38a9
Juan José Chacón <Juan Jos Chacn@DESKTOP-JUANJO>
2024-05-29 11:08:11 +0200

    pepino de mar

--- EjercicioGit2/src/identificacion/identificador.java
+++ EjercicioGit2/src/identificacion/identificador.java
@@ -7,6 +7,7 @@ public class identificador {
 		System.out.println("adios cocodrilo");
 		System.out.println("Hola pepsicola");
 		System.out.println("adios cocacola");
+		System.out.println("Pepino de mar");
 	}
 
 }
